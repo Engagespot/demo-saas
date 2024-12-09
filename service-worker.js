d2a812289da9a53233b4d42182c8e2f96743d905
Anand <anandrmedia@gmail.com>
2024-12-09 08:48:48 +0530

    fix: ios pwa

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,2 @@
-importScripts('https://cdn.engagespot.co/service-worker-v2.js');
+importScripts('https://cdn.engagespot.co/serviceWorkerv2.js');
+//importScripts('https://cdn.engagespot.co/service-worker-v2.js');
